aeeb00d2f806308af72011425c8e73ea063c50b1
HOSAMGIN <junuyea@gmail.com>
2026-03-13 10:46:19 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,2 @@
-const express = require('express');
-const app = express();
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Server is Live!' });
-});
-
-app.get('/', (req, res) => {
-  res.send('Stock Dashboard Server is running!');
-});
-
+const app = require('../artifacts/api-server/dist/index.cjs');
 module.exports = app;
